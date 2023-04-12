7a1d17e48c3e585b3bf044591128495f86effe83
bit220 <bit220@mail.ru>
2023-04-12 12:30:18 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+function App() {
+    return (
+        <div className="App">
+            <RouterProvider router={router}/>
+        </div>
+    );
+}
+
+export default App;
